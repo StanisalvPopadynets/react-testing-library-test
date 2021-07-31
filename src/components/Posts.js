2fafccb9bd312f8986e964ccd2d036d4b0eac33b
Stanislav Popadynets <popadynetss@gmail.com>
2021-07-31 15:45:51 +0300

    add tests for posts component

---
+++ src/components/Posts.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+export const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
