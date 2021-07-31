9c07d01f4965e9c48463f01e1cc96c8e6f3f534b
Stanislav Popadynets <popadynetss@gmail.com>
2021-07-31 18:45:14 +0300

    add create post functionality and cover it with tests

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,18 +1,69 @@
 import React, { useEffect, useState } from "react";
 
 export const Posts = () => {
+  const initialPostData = { title: "", body: "" };
+
   const [posts, setPosts] = useState([]);
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [newPost, setNewPost] = useState(initialPostData);
+
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((res) => res.json())
       .then((data) => setPosts(data));
   }, []);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify(newPost),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setNewPost(initialPostData);
+        setIsFormVisible(false);
+        setPosts([data, ...posts]);
+      });
+  };
+
+  const onCancel = () => {
+    setIsFormVisible(false);
+    setNewPost(initialPostData);
+  };
+
   return (
     <div>
+      {!isFormVisible && (
+        <button onClick={() => setIsFormVisible(true)}>Add New Post</button>
+      )}
+      {isFormVisible && (
+        <form onSubmit={onSubmit}>
+          <h3>New Post</h3>
+          <input
+            type="text"
+            placeholder="Title"
+            value={newPost.title}
+            onChange={(e) => setNewPost({ ...newPost, title: e.target.value })}
+          />
+          <br />
+          <textarea
+            placeholder="Body"
+            value={newPost.body}
+            onChange={(e) => setNewPost({ ...newPost, body: e.target.value })}
+          ></textarea>
+          <br />
+          <button type="submit">Submit</button>
+          <button onClick={onCancel}>Cancel</button>
+        </form>
+      )}
       <h1>Posts</h1>
       <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
+        {posts.map((post, index) => (
+          <li key={index}>
             <h3>{post.title}</h3>
             <p>{post.body}</p>
           </li>
--- src/components/Posts.test.js
+++ src/components/Posts.test.js
@@ -1,4 +1,10 @@
-import { act, render, screen } from "@testing-library/react";
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from "@testing-library/react";
 import { Posts } from "./Posts";
 
 const mockPostData = [
@@ -23,21 +29,84 @@ const mockPostData = [
   },
 ];
 
+const mockNewPostData = {
+  userId: 2,
+  id: 17,
+  title: "fugit voluptas sed molestias voluptatem provident",
+  body: "eos voluptas et aut odit natus earum aspernatur fuga molestiae ullam deserunt ratione qui eos qui nihil ratione nemo velit ut aut id quo",
+};
+
 describe("Posts", () => {
   beforeEach(() => {
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        json: () => Promise.resolve(mockPostData),
-      })
-    );
+    global.fetch = jest.fn((url, options) => {
+      if (options?.method === "POST") {
+        return Promise.resolve({
+          json: () => Promise.resolve(mockNewPostData),
+        });
+      } else
+        return Promise.resolve({
+          json: () => Promise.resolve(mockPostData),
+        });
+    });
   });
   test("fetches and renders posts", async () => {
-    await act(async () => {
-      render(<Posts />);
+    render(<Posts />);
+
+    expect(window.fetch).toHaveBeenCalledWith(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+
+    await waitFor(() => {
+      mockPostData.forEach((post) => {
+        expect(screen.getByText(post.title)).toBeInTheDocument();
+      });
+    });
+  });
+
+  test("clicking on cancel hides the form and reset to default values", async () => {
+    render(<Posts />);
+
+    await waitFor(() => fireEvent.click(screen.getByText("Add New Post")));
+
+    expect(screen.getByPlaceholderText("Title")).toBeInTheDocument();
+
+    fireEvent.change(screen.getByPlaceholderText("Title"), {
+      target: { value: "New Post Title" },
+    });
+    expect(screen.queryByPlaceholderText("Title").value).toBe("New Post Title");
+
+    fireEvent.click(screen.getByText("Cancel"));
+
+    expect(screen.queryByPlaceholderText("Cancel")).not.toBeInTheDocument();
+
+    fireEvent.click(screen.getByText("Add New Post"));
+    expect(screen.queryByPlaceholderText("Title").value).toBe("");
+  });
+
+  test("creates and renders a post after submitting the form", async () => {
+    render(<Posts />);
+
+    await waitFor(() => fireEvent.click(screen.getByText("Add New Post")));
+
+    const titleInput = screen.getByPlaceholderText("Title");
+    const bodyInput = screen.getByPlaceholderText("Body");
+    const submitButton = screen.getByRole("button", { name: "Submit" });
+
+    expect(titleInput.value).toBe("");
+    expect(bodyInput.value).toBe("");
+    expect(submitButton).toBeInTheDocument();
+
+    fireEvent.change(titleInput, { target: { value: mockNewPostData.title } });
+    fireEvent.change(bodyInput, { target: { value: mockNewPostData.body } });
+
+    await waitFor(() => {
+      fireEvent.click(submitButton);
     });
 
-    mockPostData.forEach((post) => {
-      expect(screen.getByText(post.title)).toBeInTheDocument();
-    });
+    expect(titleInput).not.toBeInTheDocument();
+    expect(bodyInput).not.toBeInTheDocument();
+
+    expect(screen.getByText(mockNewPostData.title)).toBeInTheDocument();
+    expect(screen.getByText(mockNewPostData.body)).toBeInTheDocument();
   });
 });
